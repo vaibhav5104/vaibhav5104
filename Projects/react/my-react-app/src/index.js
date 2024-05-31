aeafa2420bbb4ba3acecfb5a5b4acdd5c60607e9
Vaibhav Sharma <vaibhavsharma5104@gmail.com>
2024-06-01 00:59:33 +0530

    Add cpp files

--- Projects/react/my-react-app/src/index.js
+++ Projects/react/my-react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App1';
+import App from './App2';
 import { StrictMode } from 'react';
 // import { createRoot } from 'react-dom/client';
 // import './style.css'
