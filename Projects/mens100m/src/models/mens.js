4a02b2968460fd374db38b56b1c227da6a8c59d2
Vaibhav Sharma <vaibhavsharma5104@gmail.com>
2024-09-07 12:40:49 +0530

    add mens100m project files

---
+++ Projects/mens100m/src/models/mens.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+const menSchema = new mongoose.Schema({
+    
+    ranking:{
+        type:Number,
+        required:true,
+        unique:true,
+    },
+    name:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    dob:{
+        type:String,
+        // default:now,
+        required:true,
+        trim:true
+
+    },
+    country:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    score:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    event:{
+        type:String,
+        default:"100m"
+    },
+})
+
+// we are creating a new collection
+const MensRanking = new mongoose.model("MenRanking",menSchema)
+
+module.exports = MensRanking
